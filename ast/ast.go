faa1b9a29d2c6cbdc068da8ba2ffb3f4f97db434
devj <power3xz@gmail.com>
2022-03-23 01:52:52 +0900

    오타 수정

--- ast/ast.go
+++ ast/ast.go
@@ -167,6 +167,6 @@ type Boolean struct {
 	Value bool
 }
 
-func (b *Boolean) expressioNode()       {}
+func (b *Boolean) expressionNode()      {}
 func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
 func (b *Boolean) String() string       { return b.Token.Literal }
