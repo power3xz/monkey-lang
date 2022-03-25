230a887e1e25d607e05314e339e7bafe2e92a757
devj <power3xz@gmail.com>
2022-03-26 02:59:01 +0900

    리턴문 파싱 수정

--- parser/parser.go
+++ parser/parser.go
@@ -115,7 +115,10 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 	stmt := &ast.ReturnStatement{Token: p.curToken}
 
 	p.nextToken()
-	for !p.curTokenIs(token.SEMICOLON) {
+
+	stmt.ReturnValue = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 	return stmt
