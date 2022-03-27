3f06a619b442be24d95d81fcc4c9655cce45e087
devj <power3xz@gmail.com>
2022-03-27 15:06:27 +0900

    Integer object interface 구현

--- object/object.go
+++ object/object.go
@@ -3,6 +3,11 @@ package object
 import "fmt"
 
 type ObjectType string
+
+const (
+	INTEGER_OBJ = "INTEGER"
+)
+
 type Object interface {
 	Type() ObjectType
 	Inspect() string
@@ -12,4 +17,5 @@ type Integer struct {
 	Value int64
 }
 
-func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
