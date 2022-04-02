db658e19117f290b5ed3bb6a269577ad142c91fe
devj <power3xz@gmail.com>
2022-04-03 00:11:00 +0900

    해시 불리언 리터럴 파싱 테스트 케이스 추가

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -859,6 +859,50 @@ func TestParsingEmptyHashLiteral(t *testing.T) {
 	}
 }
 
+func TestParsingHashLiteralsBooleanKeys(t *testing.T) {
+	input := `{true: true, false: false}`
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	stmt := program.Statements[0].(*ast.ExpressionStatement)
+	hash, ok := stmt.Expression.(*ast.HashLiteral)
+	if !ok {
+		t.Fatalf("exp is not ast.HashLiteral. got=%T", stmt.Expression)
+	}
+
+	if len(hash.Pairs) != 2 {
+		t.Errorf("hash.Pairs has wrong length. got=%d", len(hash.Pairs))
+	}
+
+	tests := map[bool]func(ast.Expression){
+		true: func(e ast.Expression) {
+			testBooleanLiteral(t, e, true)
+		},
+		false: func(e ast.Expression) {
+			testBooleanLiteral(t, e, false)
+		},
+	}
+
+	for key, value := range hash.Pairs {
+		literal, ok := key.(*ast.Boolean)
+		if !ok {
+			t.Errorf("key is not ast.Boolean. got=%T", key)
+			continue
+		}
+
+		testFunc, ok := tests[literal.Value]
+		if !ok {
+			t.Errorf("No test function for key %q found", literal.String())
+			continue
+		}
+
+		testFunc(value)
+	}
+}
+
 func TestParsingHashLiteralsWithExpressions(t *testing.T) {
 	input := `{"one": 0 + 1, "two": 10 - 8, "three": 15 / 5}`
 
